20cfee9e2e1c9ed83033303283fe7b085a2dbf8e
yunaaaaaaaaaaaa <125117380+yunaaaaaaaaaaaa@users.noreply.github.com>
2025-03-27 14:37:46 +0900

    Add files via upload

---
+++ HelloWorld04/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+       String name = "서진환";
+       int age = 24;
+       System.out.println(name + "님은" + age + "살 입니다.");
+    }
+}
---
+++ HelloWorld04/src/practice.java
@@ -0,0 +1,8 @@
+public class practice {
+    public static void main(String[] args) {
+        final double PI = 3.14;
+        double radius = 10;
+        double area = (radius * radius * PI);
+        System.out.println("반지름이" + (int)radius + "인 원의 면적은" + (int)area + "이다.");
+    }
+}
